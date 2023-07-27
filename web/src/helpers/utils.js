1088e1f16d2fed650204ac3d704171a710433f3a
SunnyGPT <126902077+SunnyGPT@users.noreply.github.com>
2023-07-27 11:29:27 +0800

    Update utils.js

--- web/src/helpers/utils.js
+++ web/src/helpers/utils.js
@@ -17,7 +17,7 @@ export function isRoot() {
 
 export function getSystemName() {
   let system_name = localStorage.getItem('system_name');
-  if (!system_name) return 'OpenAI-356';
+  if (!system_name) return 'OpenAI-365';
   return system_name;
 }
 
